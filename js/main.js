8aaa7b6e8773f5a686a26061a622a400bb2ec182
verve-neowise <verve.neowise@gmail.com>
2022-12-01 18:32:19 +0500

    dsa

---
+++ js/main.js
@@ -0,0 +1,89 @@
+ //savollar
+ // 1.
+ //let,var,const, 
+ //let = ozgaruvchan ozgaruvchi,
+ //var = ozgaruvchan ozgaruvchi, 
+ //const = ozgaruvchan ozgarmas,
+
+ //2.
+ //string    = " ", ' ', ` `,
+ //number    = 1, -1, 1.1,
+ //BigInt    = 10,
+ //boolean   = true, false,
+ //object    = array, object,
+ //undefined = topilmagan, belgilanmagan,
+ //null      = b`osh, yo`q,
+ //Symbol    = simvol,
+
+ //3.
+// Increment = (++) ,
+// decrement (--),
+
+
+
+
+//4.
+// string + numbr = string,
+
+
+
+//5.
+// && = va belgisi,
+// || yokiiy,
+
+
+//6.
+// if = agar,
+// else = yokiy,
+
+//7.
+//if (a < b){
+//  console.log('hello imac'),  
+//}
+//else if(a < c){,
+//   console.log('hello chinorbek'), 
+//}
+
+//8.
+// massiv = 'massiv oid malumotlarni tartib soni boyicha saqlaydi yaniy index',
+
+//9.
+//kalit sozlar yordamida saqlanadi,
+
+//10.
+//metod o`z ichiga (push, shift, lenghth, map, unshift, toUappercase, toLovercase,).
+
+//masalalar.
+//1.
+
+//1.
+// const hello = (a,b) =>{
+// console.log(a+b)
+// }
+
+// hello(12, 12)
+ 
+
+
+
+
+
+//2.
+
+
+// let aa=2022
+// function world(a){
+// return aa - a
+
+// }
+// console.log(world(15));
+
+
+//3.
+//const hello=(a,b) =>{
+//    if (typeof(a) === "number " &&  typeof(b) === "number"){
+//   console.log(a+b);
+//  }else{
+//    console.log("son emas");
+//}}  
+//hello()
